137adcf1f1bd8d3a2319ddc0cb8d56b3aebae5ce
Juwon Park <devprofessionalism@gmail.com>
2023-02-22 20:17:36 +0900

    메소드 이름으로 쿼리 생성

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -2,6 +2,8 @@ package study.datajpa.repository;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -45,20 +47,34 @@ class MemberJpaRepositoryTest {
 
 		findMember1.setUsername("change member");
 
-		// //리스트 조회 검증
-		// List<Member> all = memberJpaRepository.findAll();
-		// assertThat(all.size()).isEqualTo(2);
-		//
-		// //카운트 검증
-		// long count = memberJpaRepository.count();
-		// assertThat(count).isEqualTo(2);
+		//리스트 조회 검증
+		List<Member> all = memberJpaRepository.findAll();
+		assertThat(all.size()).isEqualTo(2);
+
+		//카운트 검증
+		long count = memberJpaRepository.count();
+		assertThat(count).isEqualTo(2);
 
 		//삭제 검증
-		// memberJpaRepository.delete(member1);
-		// memberJpaRepository.delete(member2);
-		//
-		// long deletedCount = memberJpaRepository.count();
-		// assertThat(deletedCount).isEqualTo(0);
+		memberJpaRepository.delete(member1);
+		memberJpaRepository.delete(member2);
+
+		long deletedCount = memberJpaRepository.count();
+		assertThat(deletedCount).isEqualTo(0);
+	}
+
+	@Test
+	public void findByUsernameAndAgeGreaterThan() {
+		Member m1 = new Member("AAA", 10);
+		Member m2 = new Member("AAA", 20);
+		memberJpaRepository.save(m1);
+		memberJpaRepository.save(m2);
+
+		List<Member> result = memberJpaRepository.findByUsernameAndAgeGreaterThen("AAA", 15);
+
+		assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+		assertThat(result.get(0).getAge()).isEqualTo(20);
+		assertThat(result.size()).isEqualTo(1);
 	}
 
 }
